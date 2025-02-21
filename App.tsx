97f13b279efa02b8b7cf70328cfd69de136b9870
bruno <brunoherrera1158@gmail.com>
2025-02-21 15:30:45 -0300

    Primer commit: Subiendo mi proyecto a GitHub

--- App.tsx
+++ App.tsx
@@ -1,13 +1,10 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
-
 import './global.css';
+import Home from 'components/home';
 
 export default function App() {
   return (
     <>
-      <ScreenContent title="Home" path="App.tsx" />
-      <StatusBar style="auto" />
+      <Home />
     </>
   );
 }
--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,9 @@
+/* eslint-env node */
 const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
 
-// eslint-disable-next-line no-undef
-const config = getDefaultConfig(__dirname);
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(
+   __dirname 
+);
 
-module.exports = withNativeWind(config, { input: './global.css' });
+module.exports = config;
