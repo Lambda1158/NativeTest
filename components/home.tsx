c25d41b1bd7229dc7854064d96148241f0b78344
bruno <brunoherrera1158@gmail.com>
2025-02-21 16:22:48 -0300

    tailwind funciona

--- components/home.tsx
+++ components/home.tsx
@@ -1,12 +1,14 @@
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { Text, View } from 'react-native';
 
-export default function App() {
+export default function Home() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView className="flex-1 bg-gray-100">
+      <SafeAreaView className="flex-1 bg-gray-400">
         <View className="flex-1 items-center justify-center">
-          <Text className="text-xl font-bold text-blue-600">¡Hola, SafeArea con NativeWind!</Text>
+          <Text className="text-xl font-bold text-blue-600">
+            ¡Hola, SafeArea No funciona tailwind !
+          </Text>
         </View>
       </SafeAreaView>
     </SafeAreaProvider>
