922a2e42f99023aeddf4c2d26c8a67a754ee568d
bruno <brunoherrera1158@gmail.com>
2025-03-09 18:53:09 -0300

    Update y fix en nativewind

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
 import './global.css';
-import Home from 'components/home';
+import '@expo/metro-runtime';
+import Home from 'app/home';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-   <Home/>
+    <SafeAreaProvider>
+        <StatusBar style="auto" />
+        <Home />
+    </SafeAreaProvider>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./App.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
+  darkMode: 'class',
+  content: [
+    './App.{js,ts,tsx}', // Archivo principal
+    './app/**/*.{js,ts,tsx}', // Incluye todos los archivos en la carpeta app
+    './components/**/*.{js,ts,tsx}', // Incluye todos los archivos en la carpeta components
+  ],
 
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Colores de Spotify
+        'spotify-green': '#1DB954',
+        'spotify-black': '#191414',
+        'spotify-white': '#FFFFFF',
+        'spotify-dark-gray': '#282828',
+        'spotify-medium-gray': '#535353',
+        'spotify-light-gray': '#B3B3B3',
+        'spotify-dark-green': '#1ED760',
+        'spotify-red': '#FF0000',
+      },
+    },
   },
   plugins: [],
 };
