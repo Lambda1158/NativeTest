1a7a6d78b34a0c218ed88c8cad01209cc6c6c225
bruno <brunoherrera1158@gmail.com>
2025-03-10 19:36:10 -0300

    update

--- Front/app/_layout.tsx
+++ Front/app/_layout.tsx
@@ -1,9 +1,9 @@
 import { View, Text } from 'react-native';
-import { Stack, Slot } from 'expo-router';
+import { Stack } from 'expo-router';
 export default function Layout() {
   return (
     <View className="flex-1">
-      <Text className="mx-auto mt-20">Este es el layout</Text>
+      <Text className="mx-auto mt-20 text-spotify-green text-xl font-semibold">Este es el layout</Text>
       <Stack />
     </View>
   );
