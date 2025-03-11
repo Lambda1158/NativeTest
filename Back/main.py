ba8098800cc68129bf4d0c09c5d3db5dd3b4cc6e
bruno <brunoherrera1158@gmail.com>
2025-03-11 17:27:27 -0300

    update

--- Back/main.py
+++ Back/main.py
@@ -2,13 +2,16 @@ from flask import Flask
 from dotenv import load_dotenv
 import os
 import requests
+
+load_dotenv()
+
 app=Flask(__name__)
 
 CLIENT_ID=os.getenv('CLIENT_ID')
 CLIENT_SECRET=os.getenv('CLIENT_SECRET')
 REDIRECT_URI="http://localhost:5000/callback"
 AUTH_URL="https://accounts.spotify.com/authorize"
-TOKEN_URL="https://accounts.spotify.com/authorize/api/token"
+TOKEN_URL="https://accounts.spotify.com/api/token"
 API_BASE_URL="https://api.spotify.com/v1/"
 
 
@@ -24,4 +27,6 @@ def login():
 		 "scope":scope,
 		 "redirect_uri":REDIRECT_URI,
 		 "show_dialog":True}
-	
+	auth_url = f"{AUTH_URL}?{'&'.join([f'{key}={value}' for key, value in params.items()])}"
+	return f'<a href="{auth_url}">Login with Spotify</a>'
+
