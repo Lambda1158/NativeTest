e3f2f0f820ccea801fe64a937b718552200aacd5
bruno <brunoherrera1158@gmail.com>
2025-03-16 23:52:49 -0300

    update

--- Back/main.py
+++ Back/main.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, redirect, request, jsonify, session
 from dotenv import load_dotenv
+from datetime import datetime,timedelta
 import os
 import requests
 
@@ -21,12 +22,42 @@ def index():
 
 @app.route("/login")
 def login():
-	scope="user-read-private user-read-email"
-	params={"client_id":CLIENT_ID,
+	scope = "user-read-private user-read-email"
+	params = {"client_id":CLIENT_ID,
 		 "response_type":"code",
 		 "scope":scope,
 		 "redirect_uri":REDIRECT_URI,
 		 "show_dialog":True}
 	auth_url = f"{AUTH_URL}?{'&'.join([f'{key}={value}' for key, value in params.items()])}"
-	return f'<a href="{auth_url}">Login with Spotify</a>'
+	return redirect(auth_url)
+
+@app.route("/callback")
+def callback():
+	if "error" in request.args:
+		return jsonify({"error":request["error"]})
+	if "code" in request.args:
+		req_body ={
+			"code":request["code"],
+			"grant_type":"authorization_code",
+			"redirect_uri":REDIRECT_URI,
+			"client_id":CLIENT_ID,
+			"client_secret":CLIENT_SECRET,
+		}
+		response = request.post(TOKEN_URL,data=req_body)
+		token_info = response.json()
+		session["access_token"] = token_info["access_token"]
+		session["refresh_token"] = token_info["refresh_token"]
+		session["expires_at"] = datetime.now().timestamp() + token_info["expires_in"]
+		return redirect("/playlist")
+
+@app.route("/playlist")
+def get_playlist():
+	if "access_token" not in session:
+		return redirect("/login")
+	if datetime.now().timestamp() > session["expires_at"]:
+		return redirect("/refresh-token")
+	headers = {
+		"Authorization": f"Bearer {session["access_token"]}"
+	}
+
 
