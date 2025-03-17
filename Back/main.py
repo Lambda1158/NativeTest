f068f0b78f48ecaaa1010183e818e8d50a27d2e8
bruno <brunoherrera1158@gmail.com>
2025-03-17 15:07:00 -0300

    update

--- Back/main.py
+++ Back/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, request, jsonify, session
+from flask import Flask, redirect, request, jsonify, session, url_for
 from dotenv import load_dotenv
 from datetime import datetime,timedelta
 import os
@@ -7,6 +7,8 @@ import requests
 load_dotenv()
 
 app=Flask(__name__)
+
+app.secret_key = os.getenv('SECRET_KEY')  
 
 CLIENT_ID=os.getenv('CLIENT_ID')
 CLIENT_SECRET=os.getenv('CLIENT_SECRET')
@@ -18,32 +20,43 @@ API_BASE_URL="https://api.spotify.com/v1/"
 
 @app.route("/")
 def index():
-	return "welcome to my spotify web app"
+	url_redirect = url_for("login")
+	return  f'''
+    <h1 style="color: blue; text-align: center;">¡Bienvenido a mi aplicación!</h1>
+    <p style="text-align: center;">Este es un texto con un adorno.</p>
+    <p style="text-align: center;">
+        <a href="{url_redirect}" style="color: green; text-decoration: none;">
+            Llevame al login
+        </a>
+    </p>
+    '''
 
 @app.route("/login")
 def login():
 	scope = "user-read-private user-read-email"
-	params = {"client_id":CLIENT_ID,
+	params = {
+		"client_id":CLIENT_ID,
 		 "response_type":"code",
 		 "scope":scope,
 		 "redirect_uri":REDIRECT_URI,
-		 "show_dialog":True}
+		 "show_dialog":True
+		 }
 	auth_url = f"{AUTH_URL}?{'&'.join([f'{key}={value}' for key, value in params.items()])}"
 	return redirect(auth_url)
 
 @app.route("/callback")
 def callback():
 	if "error" in request.args:
-		return jsonify({"error":request["error"]})
+		return jsonify({"error":request.args["error"]})
 	if "code" in request.args:
 		req_body ={
-			"code":request["code"],
+			"code":request.args["code"],
 			"grant_type":"authorization_code",
 			"redirect_uri":REDIRECT_URI,
 			"client_id":CLIENT_ID,
 			"client_secret":CLIENT_SECRET,
 		}
-		response = request.post(TOKEN_URL,data=req_body)
+		response = requests.post(TOKEN_URL,data=req_body)
 		token_info = response.json()
 		session["access_token"] = token_info["access_token"]
 		session["refresh_token"] = token_info["refresh_token"]
@@ -59,5 +72,26 @@ def get_playlist():
 	headers = {
 		"Authorization": f"Bearer {session["access_token"]}"
 	}
+	response = requests.get(API_BASE_URL + "me/playlists", headers=headers)
+	playlist = response.json()
+	return jsonify(playlist)
 
-
+@app.route("/refresh-token")
+def refresh_token():
+	if "refresh_token" not in session:
+		return redirect("/login")
+	if datetime.now().timestamp() > session["expires_at"]:
+		req_body = {
+			"grant_type":"refresh_token",
+			"refresh_token":session["refresh_token"],
+			"client_id":CLIENT_ID,
+			"client_secret":CLIENT_SECRET
+		}
+		response = requests.post(TOKEN_URL, data=req_body)
+		new_token_info = response.json()
+		session["access_token"] = new_token_info["access_token"]
+		session["expires_at"] = datetime.now().timestamp() + new_token_info["expires_in"]
+		return redirect("/playlist")
+	
+if __name__ == "__main__":
+	app.run(host="0.0.0.0",debug=True)
