51e523e0eea6953f951a1bdbbf7d78ba4caddfc6
bruno <brunoherrera1158@gmail.com>
2025-03-21 12:58:55 -0300

    update

--- Back/app/__init__.py
+++ Back/app/__init__.py
@@ -1,11 +1,33 @@
-# app/__init__.py
-from flask import Flask
+from flask import Flask, session
 from config import Config
+from flask_caching import Cache
+from spotipy import Spotify 
+from spotipy.oauth2 import SpotifyOAuth 
+from spotipy.cache_handler import FlaskSessionCacheHandler  
+
+
+scope = "user-read-private user-read-email user-top-read"
+params = {
+    "client_id": Config.CLIENT_ID,
+    "response_type": "code",
+    "scope": scope,
+    "redirect_uri": Config.REDIRECT_URI,
+    "show_dialog": True
+}
+cache_handler = FlaskSessionCacheHandler(session=session)
+sp_oauth = SpotifyOAuth(client_id=Config.CLIENT_ID,client_secret=Config.CLIENT_SECRET,scope=scope,redirect_uri=Config.REDIRECT_URI,cache_handler=cache_handler)
+sp = Spotify(auth_manager=sp_oauth)
+
+cache = Cache()
 
 def create_app():
     app = Flask(__name__)
+    
     app.config.from_object(Config)
-
+    app.config["CACHE_TYPE"] = "SimpleCache"
+    app.config["CACHE_DEFAULT_TIMEOUT"] = 300
+    cache.init_app(app)
+    
     # Registra los blueprints (rutas)
     from app.routes.auth import auth_bp
     from app.routes.spotify import spotify_bp
--- Back/app/routes/spotify.py
+++ Back/app/routes/spotify.py
@@ -1,25 +1,22 @@
 # app/routes/spotify.py
-from flask import Blueprint, jsonify, redirect, session, url_for
+from flask import Blueprint, jsonify, session, url_for, redirect
 import requests
-from datetime import datetime
 from config import Config
-
+from app import cache
+from app.lib.middleware import token_required
+from app import sp
 spotify_bp = Blueprint('spotify', __name__)
 
 @spotify_bp.route("/top-tracks")
+@cache.cached(timeout=300)
+@token_required
 def get_top_tracks():
-    if "access_token" not in session:
-        return redirect(url_for("auth.login"))
-    if datetime.now().timestamp() > session["expires_at"]:
-        return redirect(url_for("auth.refresh_token"))
-
     headers = {
         "Authorization": f"Bearer {session['access_token']}"
     }
-
     params = {
         "time_range": "short_term",  # Últimas 4 semanas
-        "limit": 10  # Número de tracks a obtener
+        "limit": 15  # Número de tracks a obtener
     }
     top_tracks_response = requests.get(Config.API_BASE_URL + "me/top/tracks", headers=headers, params=params)
     top_tracks = top_tracks_response.json()
@@ -27,28 +24,13 @@ def get_top_tracks():
     return jsonify(top_tracks)
 
 @spotify_bp.route("/playlist")
+@token_required
 def get_playlist():
-	if "access_token" not in session:
-		return redirect("/login")
-	if datetime.now().timestamp() > session["expires_at"]:
-		return redirect("/refresh-token")
-	headers = {
-		"Authorization": f"Bearer {session["access_token"]}"
-	}
-	response = requests.get(Config.API_BASE_URL + "me/playlists", headers=headers)
-	playlist = response.json()
-	return jsonify(playlist)
+    playlists = sp.current_user_playlists()
+    return jsonify(playlists)
+    
+@spotify_bp.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("auth.home"))
 
-
-@spotify_bp.route("/")
-def index():
-	url_redirect = url_for("auth.login")
-	return  f'''
-    <h1 style="color: blue; text-align: center;">¡Bienvenido a mi aplicación!</h1>
-    <p style="text-align: center;">Este es un texto con un adorno.</p>
-    <p style="text-align: center;">
-        <a href="{url_redirect}" style="color: green; text-decoration: none;">
-            Llevame al login
-        </a>
-    </p>
-    '''
