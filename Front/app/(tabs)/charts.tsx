0d2d4129ee234a8037091adde9fa50c699bf573d
bruno <brunoherrera1158@gmail.com>
2025-03-22 21:08:19 -0300

    update en el front

--- Front/app/(tabs)/charts.tsx
+++ Front/app/(tabs)/charts.tsx
@@ -2,7 +2,7 @@ import { Text, View } from 'react-native';
 
 export default function Charts() {
   return (
-    <View className='flex-1 items-center mt-10'>
+    <View className='flex-1 items-center mt-10 bg-spotify-black'>
       <Text className='text-spotify-green text-2xl font-bold'>Estos son los charts</Text>
     </View>
   );
--- Front/app/home.tsx
+++ Front/app/home.tsx
@@ -5,7 +5,7 @@ import { Link } from 'expo-router';
 export default function Home() {
   const insets = useSafeAreaInsets();
   return (
-    <View className="flex-1 items-center justify-center ">
+    <View className="flex-1 items-center justify-center bg-black ">
       <Text className="text-2xl font-bold text-spotify-green">
         Esta es una app de Spotify Wrapped
       </Text>
